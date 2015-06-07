dfbcb5814abc8763542c2978eb02488c1a0668ce
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2015-06-07 11:15:49 -0700

    Add root route

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', function(request, response) {
+  response.send(Date() + ". It's working (state:" + state.toString() + ").");
+});
+
 var state = 0
 app.get('/deploy', function(request, response) {
   response.send("state:" + state.toString());
