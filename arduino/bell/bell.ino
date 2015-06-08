a761e5535efbf797ea1e4c50f88a8a3bd36ed124
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2015-06-07 17:38:11 -0700

    Use /status for status

--- arduino/bell/bell.ino
+++ arduino/bell/bell.ino
@@ -7,13 +7,14 @@
 #define ESP_SINGLE_CONNECTION_CMD "AT+CIPMUX=0"                     // configure for single connection
 #define ESP_TCP_START_CMD "AT+CIPSTART=\"TCP\",\"10.0.0.2\",5000\r" // connect with my laptop
 #define ESP_TCP_SEND_CMD "AT+CIPSEND=18\r"                          // will send 18 bytes
-#define ESP_TCP_GET_CMD "GET /deploy HTTP/1.0\r\n"                  // sent a GET /
+#define ESP_TCP_GET_CMD "GET / HTTP/1.0\r\n"                  // sent a GET /
 
-#define LED_PIN 8
+#define LED_PIN 13
 #define BELL_PIN 9
 
 #define BLINK_ERROR 10
-#define BLINK_OK 2
+#define BLINK_OK 3
+#define BLINK_SETUP 1
 
 Servo bell;
 
@@ -22,31 +23,34 @@ void setup() {
 
   sendData(ESP_RESET_CMD, 3000);                                  // reset module
   pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
   bell.attach(BELL_PIN);
+
+  delay(5000);
+  blink(BLINK_SETUP);
 }
 
 void loop() {
   String response;
 
+  delay(2000);
   sendData(ESP_TCP_START_CMD, 1000);
-  if (response.indexOf("Error")) return blink(BLINK_ERROR);
+  if (response.indexOf("ERROR") != -1) return blink(BLINK_ERROR);
 
-  delay(1000);
+  delay(2000);
   sendData(ESP_TCP_SEND_CMD, 1000);
-  if (response.indexOf("Error")) return blink(BLINK_ERROR);
+  if (response.indexOf("ERROR") != -1) return blink(BLINK_ERROR);
 
-  delay(1000);
-  response = sendData(ESP_TCP_GET_CMD, 3000);
-  if (response.indexOf("Error")) return blink(BLINK_ERROR);
-
+  delay(2000);
+  response = sendData(ESP_TCP_GET_CMD, 5000);
+  if (response.indexOf("ERROR") != -1) return blink(BLINK_ERROR);
+  
+  blink(BLINK_OK);
   if (response.indexOf("state:1") != -1) {
-    blink(BLINK_OK);
     ring();
-  } else {
-    blink(BLINK_ERROR);
   }
 
-  delay(5000);
+  delay(8000);
 }
 
 String sendData(String command, const int timeout) {
--- index.js
+++ index.js
@@ -4,17 +4,17 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
+app.get('/status', function(request, response) {
   response.send(Date() + ". It's working (state:" + state.toString() + ").");
 });
 
 var state = 0
-app.get('/deploy', function(request, response) {
+app.get('/', function(request, response) {
   response.send("state:" + state.toString());
   state = 0
 });
 
-app.post('/deploy', function(request, response) {
+app.post('/', function(request, response) {
   state = 1
   response.send("state:" + state.toString());
 });
